5f8e93fb25c8f5dfb7481d4b72f3c6863f5cf273
Yuanke Tsai <yuanke.tsai@gmail.com>
2021-03-19 10:30:28 +0800

    3

--- age_drive.py
+++ age_drive.py
@@ -9,17 +9,15 @@ This is a temporary script file.
 drive = input("你有開車嗎？(y/n)")
 if drive != 'y' and drive != 'n':
     print('輸入錯誤')
-    #可在這裡寫 raise SystemExit 做中斷程式
-elif drive == 'y':
-    age = input("請問年齡？")
-    age = int(age)
+    raise SystemExit
+age = input("請問年齡？")
+age = int(age)
+if drive == 'y':
     if age < 18:
         print('你無照喔')
     else:
         print('所以你有駕照')
 elif drive == 'n':
-    age = input("請問年齡？")
-    age = int(age)
     if age < 18:
         print('那就等到成年在考駕照')
     else:
